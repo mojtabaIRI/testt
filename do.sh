d54a7505ead5c0ec7694e7c6243ba574ee6619a4
mojtabaIRI <78437728+mojtabaIRI@users.noreply.github.com>
2022-05-12 17:12:48 +0430

    Update do.sh

--- do.sh
+++ do.sh
@@ -2,6 +2,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt update 
 sudo apt install google-chrome-stable -y
+sudo apt install screen -y
 pip install webdriver-manager
 pip install selenium
 wget -O file.py https://raw.githubusercontent.com/mojtabaIRI/testt/main/ok.py?token=GHSAT0AAAAAABTG5FJZ43MCKB5NOQ26HZXUYT477YA
